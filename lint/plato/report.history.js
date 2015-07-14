0c68444f6f43b99d947a366b94016d8a083e0ae5
uupaa <uupaa.js@gmail.com>
2015-07-14 09:23:02 +0900

    duck typing

---
+++ lint/plato/report.history.js
@@ -0,0 +1,13 @@
+__history = [
+  {
+    "date": "Tue, 14 Jul 2015 00:20:45 GMT",
+    "total": {
+      "sloc": 652,
+      "maintainability": 96.21497019583859
+    },
+    "average": {
+      "sloc": 326,
+      "maintainability": "48.11"
+    }
+  }
+]
